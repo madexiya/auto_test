0a54c7403892bfa16e477ab3224da9f2f23f5309
madexiya <943647668@qq.com>
2022-11-30 15:31:20 +0800

    pytest-ordering插件指定用例运行顺序

--- pytest_live/test_calc.py
+++ pytest_live/test_calc.py
@@ -1,6 +1,15 @@
 import pytest
+import yaml
 
 from pytest_live.calculator import Calculator
+
+
+def get_datas(level):
+    with open("datas.yaml", encoding="utf-8") as f:
+        file_data = yaml.safe_load(f)
+    add_datas = file_data.get("add").get(level).get("datas")
+    add_ids = file_data.get("add").get(level).get("ids")
+    return add_datas, add_ids
 
 
 class TestCalc:
@@ -16,37 +25,33 @@ class TestCalc:
     def teardown(self):
         print("【结束计算】")
 
-    @pytest.mark.parametrize("a,b,result",
-                             [(1, 1, 2),
-                              (-0.01, 0.02, 0.01),
-                              (10, 0.02, 10.02)
-                              ], ids=["整数", "浮点型", "整形+浮点型"])
+    @pytest.mark.parametrize("a,b,result", get_datas("p0")[0], ids=get_datas("p0")[1])
     @pytest.mark.p0
+    @pytest.mark.run(order=3)
+    # p0有效边界值
     def test_add1(self, a, b, result):
         assert self.calc.add(a, b) == result
 
-    @pytest.mark.parametrize("a,b,result",
-                             [(98.99, 99, 197.99),
-                              (99, 98.99, 197.99),
-                              (-98.99, -99, -197.99),
-                              (-99, - 98.99, -197.99)])
-    @pytest.mark.p1
+    @pytest.mark.parametrize("a,b,result", get_datas("p1_1")[0], ids=get_datas("p1_1")[1])
+    @pytest.mark.p1_1
+    @pytest.mark.run(order=1)
     def test_add2(self, a, b, result):
         assert self.calc.add(a, b) == result
 
-    @pytest.mark.parametrize("a,b,result",
-                             [(99.01, 0, "参数大小超出范围"),
-                              (-99.01, -1, "参数大小超出范围"),
-                              (2, 99.01, "参数大小超出范围"),
-                              (1, -99.01, "参数大小超出范围")])
-    @pytest.mark.p1
-    def test_add3(self, a, b, result):
-        assert self.calc.add(a, b) == result
+    @pytest.mark.parametrize("a,b,error_type", get_datas("p1_2")[0], ids=get_datas("p1_2")[1])
+    @pytest.mark.p1_2
+    @pytest.mark.run(order=2)
+    # 异常处理
+    def test_add3(self, a, b, error_type):
+        with pytest.raises(eval(error_type)) as e:
+            """error_type需要传入的是一个异常类型，传入字符串会报错，使用eval可以将字符串里的当做原始类型"""
+            self.calc.add(a, b)
+        print(e.typename)
 
-    @pytest.mark.parametrize("a,b,error_type",
-                             [("文", 0, "TypeError"),
-                              (4, "字", "TypeError")])
-    @pytest.mark.p1
+    @pytest.mark.parametrize("a,b,error_type", get_datas("p2")[0], ids=get_datas("p2")[1])
+    @pytest.mark.p2
+    @pytest.mark.run(order=4)
+    # 异常处理
     def test_add4(self, a, b, error_type):
         # 1、使用try except 捕获正常的异常TypeError
         # try:
