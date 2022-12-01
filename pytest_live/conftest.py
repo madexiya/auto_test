4f486e03823d9ffce3d4467b3b2f64872ac00280
madexiya <943647668@qq.com>
2022-12-01 12:30:54 +0800

    添加测试报告

--- pytest_live/conftest.py
+++ pytest_live/conftest.py
@@ -1,3 +1,12 @@
+import time
+
+import pytest
+
+from pytest_live.calculator import Calculator
+
+
+# conftest.py文件必须放在package下面才生效
+# 作用域：先找当前同级目录的，没有就找上级目录的
 def pytest_collection_modifyitems(items):
     """
     测试用例收集完成时，将收集到的item的name和nodeid的中文显示
@@ -6,3 +15,28 @@ def pytest_collection_modifyitems(items):
     for item in items:
         item.name = item.name.encode("utf-8").decode("unicode_escape")
         item._nodeid = item.nodeid.encode("utf-8").decode("unicode_escape")
+
+
+@pytest.fixture(scope="class")
+def get_calc():
+    calc = Calculator()
+    # return calc
+    yield calc
+    print("测试结束")
+
+
+@pytest.fixture(autouse=True)
+def calc_fix():
+    print("开始计算")
+    yield
+    print("结束计算")
+
+
+@pytest.fixture(scope="session", autouse=True)
+def manage_logs(request):
+    """每次执行用例，重新按时间命名生成一个日志文件"""
+    now = time.strftime("%Y-%m-%d %H-%M-%S")
+    log_name = 'output/log/' + now + '.logs'
+    # request是pytest中内置fixture，固定写法
+    request.config.pluginmanager.get_plugin("logging-plugin") \
+        .set_log_path(log_name)
--- pytest_live/test_calc_with_fixture.py
+++ pytest_live/test_calc_with_fixture.py
@@ -1,7 +1,8 @@
+import logging
+
+import allure
 import pytest
 import yaml
-
-from pytest_live.calculator import Calculator
 
 
 def get_datas(level):
@@ -12,49 +13,42 @@ def get_datas(level):
     return add_datas, add_ids
 
 
-@pytest.fixture(scope="class")
-def get_calc():
-    calc = Calculator()
-    # return calc
-    yield calc
-    print("测试结束")
-
-
-@pytest.fixture(autouse=True)
-def calc_fix():
-    print("开始计算")
-    yield
-    print("结束计算")
-
-
+@allure.feature("计算器用例")
 class TestCalc:
     @pytest.mark.parametrize("a,b,result", get_datas("p0")[0], ids=get_datas("p0")[1])
     @pytest.mark.p0
     @pytest.mark.run(order=3)
-    # p0有效边界值
+    @allure.story("p0有效边界值")
     def test_add1(self, a, b, result, get_calc):
-        assert get_calc.add(a, b) == result
+        with allure.step("测试相加方法"):
+            logging.info(f"参数{a},{b},期望结果：{result}")
+        with allure.step("断言"):
+            assert get_calc.add(a, b) == result
 
     @pytest.mark.parametrize("a,b,result", get_datas("p1_1")[0], ids=get_datas("p1_1")[1])
     @pytest.mark.p1_1
     @pytest.mark.run(order=1)
+    @allure.story("p1_1有效边界值+无效边界值")
     def test_add2(self, a, b, result, get_calc):
+        allure.attach.file("test_image.png", "测试图片", attachment_type=allure.attachment_type.PNG, extension=".png")
         assert get_calc.add(a, b) == result
 
     @pytest.mark.parametrize("a,b,error_type", get_datas("p1_2")[0], ids=get_datas("p1_2")[1])
     @pytest.mark.p1_2
     @pytest.mark.run(order=2)
-    # 异常处理
+    @allure.story("p1_2异常处理")
     def test_add3(self, a, b, error_type, get_calc):
-        with pytest.raises(eval(error_type)) as e:
-            """error_type需要传入的是一个异常类型，传入字符串会报错，使用eval可以将字符串里的当做原始类型"""
-            get_calc.add(a, b)
-        print(e.typename)
+        with allure.step("捕获异常"):
+            with pytest.raises(eval(error_type)) as e:
+                """error_type需要传入的是一个异常类型，传入字符串会报错，使用eval可以将字符串里的当做原始类型"""
+                get_calc.add(a, b)
+        with allure.step("打印异常名称"):
+            print(e.typename)
 
     @pytest.mark.parametrize("a,b,error_type", get_datas("p2")[0], ids=get_datas("p2")[1])
     @pytest.mark.p2
     @pytest.mark.run(order=4)
-    # 异常处理
+    @allure.story("p2异常处理")
     def test_add4(self, a, b, error_type, get_calc):
         # 1、使用try except 捕获正常的异常TypeError
         # try:
